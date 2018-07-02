af67709e8e307ab1c21b7065b40d7cd1a499330b
Cary Li <carysyd@gmail.com>
2018-07-02 03:28:36 +0000

    Added kubeadm script to setup local server as a single-node K8s server

---
+++ system/kubernetes/kubeadm/single-node-k8s-master.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install kubeadm, kubelet and kubectl
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+
+# kubeadm enforces swap off before initializing K8s
+sudo swapoff -a
+
+# Initialize master node
+kubeadm init
+
+# Allow non-root user to use kubtctl
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# Install Pod Network add-on
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
+# Master isolation so pods can be run on master node
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
