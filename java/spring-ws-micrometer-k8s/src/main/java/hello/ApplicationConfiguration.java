f46dbb45ddccde666e870194fb292d62a6eed0c7
Cary Li <cary.li@purestorage.com>
2018-11-17 20:26:56 -0800

    Removed unnecessary metric registry that's already defined in actuator, also update properties to report to local StatsD server

--- java/spring-ws-micrometer-k8s/src/main/java/hello/ApplicationConfiguration.java
+++ java/spring-ws-micrometer-k8s/src/main/java/hello/ApplicationConfiguration.java
@@ -5,33 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.micrometer.core.aop.TimedAspect;
-import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.statsd.StatsdConfig;
-import io.micrometer.statsd.StatsdFlavor;
-import io.micrometer.statsd.StatsdMeterRegistry;
 
 @Configuration
 public class ApplicationConfiguration {
-
-    @Bean
-    public MeterRegistry meterRegistry() {
-        StatsdConfig config = new StatsdConfig() {
-            @Override
-            public String get(String k) {
-                return null;
-            }
-
-
-            @Override
-            public StatsdFlavor flavor() {
-                return StatsdFlavor.DATADOG;
-            }
-        };
-
-        MeterRegistry registry = new StatsdMeterRegistry(config, Clock.SYSTEM);
-        return registry;
-    }
 
     @Bean
     @Autowired
