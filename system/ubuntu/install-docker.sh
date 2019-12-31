c083a05bb872e7bd24d52d09426216e254165010
Cary Li <carysyd@gmail.com>
2019-12-30 18:12:26 -0800

    Updated docker install script on Ubuntu for 19.10

--- system/ubuntu/install-docker.sh
+++ system/ubuntu/install-docker.sh
@@ -1,25 +1,29 @@
 #!/usr/bin/env bash
 
-sudo apt-get remove docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
-sudo apt-get -y install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
+    gnupg-agent \
     software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
+# docker.com eoan repo doesn't exist yet, so cannot use lsb_release here,
+# hard code it to disco
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
+   disco \
    stable"
 
 sudo apt-get update
 
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 
+sudo docker run hello-world
