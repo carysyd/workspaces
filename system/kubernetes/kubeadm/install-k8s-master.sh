15d950435f55482f9c150b1aee50c20091b3163a
Cary LI <caryli@apple.com>
2022-03-27 14:13:35 -0700

    Updated script to install Kuberentes master with containerd

--- system/kubernetes/kubeadm/install-k8s-master.sh
+++ system/kubernetes/kubeadm/install-k8s-master.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 
 # Install kubeadm, kubelet and kubectl
-apt-get update && apt-get install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+# install containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Setup required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+sudo apt-get update && sudo apt-get install -y containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
 
 # kubeadm enforces swap off before initializing K8s, also permanently disable swap partition
 sudo swapoff -a
-sed -i 's/^.*swap.img/#&/' /etc/fstab
+sudo sed -i 's/^.*swap.img/#&/' /etc/fstab
 
 # Initialize master node
-kubeadm init --pod-network-cidr=172.16.0.0/12
+sudo kubeadm init --pod-network-cidr=172.16.0.0/12
 
 # Allow non-root user to use kubtctl
 mkdir -p $HOME/.kube
@@ -22,9 +46,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Install Pod Network add-on, at the end of this line, it would print the join node commands
-kubectl apply -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml
-
-# An networking step I found needed on Kubernetes Worker Nodes
-# Make a backup of the resolve.conf file, then use the systemd one
-mv /etc/resolv.conf /etc/resolv.conf.orig
-ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+kubectl apply -f https://projectcalico.docs.tigera.io/manifests/calico.yaml
